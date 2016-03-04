725fcc06e0a8516ed82257a04f194401feed8b10
Tom Finn <dolbyac3@gmail.com>
2016-03-04 02:35:37 -0600

    guide list changes

--- views/pages/guide/guides.js
+++ views/pages/guide/guides.js
@@ -7,29 +7,35 @@ var categoryTag = require('../../../controllers/components/tag/categorytag.js');
 
 module.exports = function(vm) {
     return layout([
-        m('.tags.current-tags', _.map(vm.tags, function(tag) {
-            return tag.id == vm.category.id ?
-                null :
-                m('a.tag.current', {
-                    href: vm.removeTag(tag),
-                    config: m.route
-                }, [
-                    m('i.fa.fa-times'), tag.name
-                ]);
-        })),
-        m('.tags.add-tags', _.map(vm.childTags, function(tag) {
-            return vm.hasTag(tag) ?
-                null :
-                tag.leaf ?
-                    m('a.tag', {
-                        href: vm.addTag(tag),
+        m('.category-header', [
+            m('a.btn.btn-success.new-guide', {
+                href: '/guide/edit?catg=' + vm.category.id,
+                config: m.route
+            }, [m('i.fa.fa-plus'), ' New Guide']),
+            m('h2', vm.category.name),
+            m('.tags.current-tags', _.map(vm.tags, function(tag) {
+                return tag.id == vm.category.id ?
+                    null :
+                    m('a.tag.current', {
+                        href: vm.removeTag(tag),
                         config: m.route
                     }, [
-                        m('i.fa.fa-plus'), tag.name
-                    ]) :
-                    m.component(categoryTag, { tag: tag, addFunc: vm.addTag, context: vm });
-        })),
-        m('h4', 'Guides'),
+                        m('i.fa.fa-times'), tag.name
+                    ]);
+            })),
+            m('.tags.add-tags', _.map(vm.childTags, function(tag) {
+                return vm.hasTag(tag) ?
+                    null :
+                    tag.leaf ?
+                        m('a.tag', {
+                            href: vm.addTag(tag),
+                            config: m.route
+                        }, [
+                            m('i.fa.fa-plus'), tag.name
+                        ]) :
+                        m.component(categoryTag, { tag: tag, addFunc: vm.addTag, context: vm });
+            }))
+        ]),
         m('.card-deck.guides', _.map(vm.guides, function(guide) {
             return m('a.card', {
                 href: '/guide/' + guide.id + '-' + slug(guide.title),
@@ -42,13 +48,13 @@ module.exports = function(vm) {
                 }),
                 m('.card-block', [
                     m('h4.card-title', guide.title),
-                    m('p.card-text', guide.author_name)
+                    m('p.card-text.author', [
+                                      m('i.fa.fa-user'),
+                                      guide.author_name
+                                      ])
                 ])
             ]);
-        })),
-        m('a.btn.btn-success.btn-lg', {
-            href: '/guide/edit?catg=' + vm.category.id,
-            config: m.route
-        }, [m('i.fa.fa-new'), ' New Guide'])
+        }))
+
     ]);
 };
