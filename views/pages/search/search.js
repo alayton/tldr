3ac8da755032573a91f61928b9c9693b2b00ea5c
alayton <an.adept@gmail.com>
2016-03-13 21:49:44 -0500

    linking to results in search helps

--- views/pages/search/search.js
+++ views/pages/search/search.js
@@ -9,13 +9,14 @@ module.exports = function(vm) {
     return layout([
         m('.text-muted', [(vm.result.page > 1 ? 'Page ' + vm.result.page + ' of ' : ''), vm.result.total_results, ' results']),
         m('.results', _.map(vm.result.guides, function(g) {
+            var url = '/guide/' + g.id + '-' + slug(g.title);
             return m('.guide', [
-                m('img', {
+                m('a', { href: url, config: m.route }, m('img', {
                     src: g.image_id ? imageurl(g.image_id, 200, 150) : 'http://lorempixel.com/200/150/cats/' + ((g.id % 10) + 1) + '/',
                     width: 160,
                     height: 120
-                }),
-                m('h3', g.title),
+                })),
+                m('a', { href: url, config: m.route }, m('h3', g.title)),
                 m('var', ['Last updated ', m('abbr', { title: moment(g.edited).format('lll') }, moment(g.edited).fromNow())]),
                 m('a.tag.category', {
                     href: '/guides/' + g.category_id + '-' + slug(g.category_name),
