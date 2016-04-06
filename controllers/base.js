f839af0b8b045563c1e59cb9387946383e62f34f
alayton <an.adept@gmail.com>
2016-04-05 21:32:03 -0500

    always show new guide button, add og:image on all pages

--- controllers/base.js
+++ controllers/base.js
@@ -1,6 +1,7 @@
-var req = require('../util/request.js');
-var layout = require('../views/layout/skeleton.js');
-var auth = require('../models/auth.js');
+var req = require('util/request.js');
+var ogImage = require('util/page/og-image.js');
+var layout = require('views/layout/skeleton.js');
+var auth = require('models/auth.js');
 
 var base = function(controller) {
     return function(params, done) {
@@ -19,6 +20,11 @@ var base = function(controller) {
 
         var vm = controller(params, done);
         vm._reqs = {};
+
+        if (!vm.ogImage) {
+            ogImage(vm);
+        }
+
         return vm;
     };
 };
